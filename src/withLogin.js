acda168de4cc36ed542b4431096e7bc4cdb1081c
erwanledoux <lawanledoux@gmail.com>
2019-04-22 20:43:30 +0200

    used with react tedux login

--- src/withLogin.js
+++ src/withLogin.js
@@ -1,12 +1,11 @@
 import { connect } from 'react-redux'
-import { compose } from 'redux'
 import _withLogin from 'with-react-login'
 
 import mapStateToProps from './mapStateToProps'
 
-export const withLogin = config => compose(
-  connect(mapStateToProps),
-  _withLogin(config)
-)
+export const withLogin = config =>
+  _withLogin(
+    Object.assign({}, config, { withDispatcher: connect(mapStateToProps) })
+  )
 
 export default withLogin
